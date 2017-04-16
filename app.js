98d3c0643c2a6f960c8f107e245c480d155bf605
Julie Jiang <juliejiang_12@hotmail.com>
2017-04-16 15:06:16 -0400

    created schema api in app.js for endGame function in Phaser

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var path = require('path');
+var path    = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var logger  = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +13,11 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.set('post', 3000);
+app.listen(app.get('port'), function() {
+    console.log('app.js is running on port', app.get('port'));
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,22 +30,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+
+app.post('/submit', function(request, response) {
+    console.log("post submit");
+    var origin = request.get('origin'); 
+    response.header('Access-Control-Allow-Origin', origin);
+    response.header("Access-Control-Allow-Headers", "X-Requested-With");
+    response.header('Access-Control-Allow-Headers', 'Content-Type');  
+
+    var username = request.body.username;
+    var email    = request.body.email;
+    var score    = parseInt(request.body.score);
+    if (username === undefined || email === undefined || score === undefined) {
+        response.send({"error":"Whoops, something is wrong with your data!"});
+    } 
+
+    var user = require('./models/leaders');
+    console.log("required schema");
+
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+
 
 module.exports = app;
