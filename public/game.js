c42f4078f56f6fe962990c590d7a2abd5fbf43d1
Uku-Kaspar Uustalu <ukukaspar.uustalu@gmail.com>
2017-05-02 11:08:07 -0400

    fixed cars and made game easier, ALL CODE NOW IN GAME.JS

--- public/game.js
+++ public/game.js
@@ -4,7 +4,7 @@ var game = new Phaser.Game(w, h, Phaser.AUTO, '', { preload: preload, create: cr
 
 var timer; var timerText;
 var player; var q1; var q2; var finish; var background; var barrier; var map;
-var q1asked = false; var q2asked = false;
+var q1asked = false; var q2asked = false; finished = false;
 var score = 0;
 
 var playerBodyScaleX = 0.6;	// resize player hitbox width
@@ -19,22 +19,24 @@ var q2x = 2650;
 var cars; // car group
 var car; // current car instance
 
+var carTimer = 0; // unlink car creation from score counter
+
 var carBodyScaleX = 0.8;		// resize car hitbox width
 var carBodyScaleY = 1;	// resize car hitbox height
 
 // assign velocity, interval (in whole seconds), and group to each car type
 // 0 = texture atlas, 1 = image, 2 = animated spritesheet
 // also manually insert image heights and widths to simplify future life
-var carTypes = {'ambulance': {velocity: 300, interval: 17, group: 2, h: 104, w: 51},
-				'audi': {velocity: 150, interval: 3, group: 0, h: 107, w: 49},
-				'bus': {velocity: 50, interval: 8, group: 1, h: 193, w: 60},
-				'car': {velocity: 120, interval: 3, group: 0, h: 109, w: 46},
-				'pickup': {velocity: 100, interval: 4, group: 0, h: 102, w: 56},
-				'police': {velocity: 300, interval: 29, group: 2, h: 107, w: 49},
-				'taxi': {velocity: 180, interval: 2, group: 0, h: 112, w: 57},
-				'truck': {velocity: 30, interval: 16, group: 1, h: 185, w: 58},
-				'van': {velocity: 80, interval: 5, group: 0, h: 98, w: 47},
-				'viper': {velocity: 200, interval: 2, group: 0, h: 110, w: 54}};
+var carTypes = {'ambulance': {velocity: 250, interval: 27, group: 2, h: 104, w: 51},
+				'audi': {velocity: 120, interval: 4, group: 0, h: 107, w: 49},
+				'bus': {velocity: 30, interval: 13, group: 1, h: 193, w: 60},
+				'car': {velocity: 100, interval: 5, group: 0, h: 109, w: 46},
+				'pickup': {velocity: 80, interval: 5, group: 0, h: 102, w: 56},
+				'police': {velocity: 250, interval: 41, group: 2, h: 107, w: 49},
+				'taxi': {velocity: 160, interval: 3, group: 0, h: 112, w: 57},
+				'truck': {velocity: 10, interval: 29, group: 1, h: 185, w: 58},
+				'van': {velocity: 60, interval: 6, group: 0, h: 98, w: 47},
+				'viper': {velocity: 180, interval: 3, group: 0, h: 110, w: 54}};
 
 // car slots based on current map, DO NOT MODIFY unless map changed
 var carSlotX = [225, 290, 385, 480, 545, 640, 735, 800, 895, 990, 1055,
@@ -85,13 +87,13 @@ function initiateCars() {
 	}
 }
 
-// NEEDS SCORE = SECONDS ELAPSED
 function updateCars() {
+	carTimer++;
 	for (i = 0; i < slots; i++) {
 
 		carType = carSlotType[i];
 
-		if((score % carTypes[carType].interval) == 0) {
+		if((carTimer % carTypes[carType].interval) == 0) {
 			if(carSlotVector[i] == 1){
 				y = -carTypes[carType].h
 			} else {
@@ -152,8 +154,11 @@ function preload() {
     game.load.image('roadTiles', 'models/roads/preview.jpg');
     game.load.image('grassTiles', 'models/roads/grass.png');
     game.load.spritesheet('student', 'assets/student.png', 50, 50);
-    game.load.image('answeroption', 'assets/nr-cone.jpg');
+    game.load.image('answeroption', 'assets/question_backdrop.jpg');
+    game.load.image('norman', 'assets/nr-cone.jpg');
+    game.load.image('ming', 'assets/ming.jpg');
     game.load.spritesheet('event', 'assets/event.png', 30, 512);
+    game.load.image('dewick', 'assets/dewick.jpg');
 
     // LOAD CARS
     game.load.spritesheet('ambulance', 'assets/ambulance.png', 51, 104);
@@ -187,6 +192,9 @@ function create() {
     q1     = game.add.sprite(q1x, 0, 'event');
     q2     = game.add.sprite(q2x, 0, 'event');
     finish = game.add.sprite(4080, 0, 'event');
+
+    dewick = game.add.sprite(4120, 64, 'dewick');
+    dewick.scale.setTo(.57, .57);
 
     q2.alpha = 0;
     q1.alpha = 0;
@@ -239,28 +247,30 @@ function updateScore(){
 
 function endGame(){
     // Print "Game Over! \n High Scores"
-    var style = { font: "50px Arial", fill: "#000000",
-		  align: "center" };
+    var style = { font: "50px Arial", fill: "#ffffff", align: "center" };
     var text = "Game Over!\n High scores:";
-    game.add.text(game.world.centerX-150, game.world.centerY-200,
-		  text, style);
+    t = game.add.text(game.world.centerX-150, game.world.centerY-400,
+                      text, style);
     t.fixedToCamera = true;
-    t.cameraOffset.setTo(220, 200);
+    t.cameraOffset.setTo(220, 70);
     game.camera.follow(null);
     
-    style = { font: "25px Arial", fill: "#000000", align: "center" };
+    style = { font: "25px Arial", fill: "#ffffff", align: "center" };
     
     // Send XMLHttpRequest to database with current user info
     // and retrieve the top 10 high scores
     loadData(function(responseText) {
-	var data = JSON.parse(responseText).data;
-	// Print each instance of high score
-	for (var i = 0; i < data.length; i++) {
-	    text = (i+1) + ". " + data[i].username + " " +
-		   data[i].email + " " + data[i].score + "\n";
-	    game.add.text(game.world.centerX-150,
-			  game.world.centerY-90+i*25, text, style);
-	}
+        var data = JSON.parse(responseText).data;
+        // Print each instance of high score
+        for (var i = 0; i < data.length; i++) {
+            text = (i+1) + ". " + data[i].username + " " +
+            data[i].email + " " + data[i].score + "\n";
+            t = game.add.text(game.world.centerX-150,
+                          game.world.centerY-90+i*25, text, style);
+            t.fixedToCamera = true;
+            t.cameraOffset.setTo(200, 200+i*30);
+            game.camera.follow(null);
+        }
     });
 }
 // Send an XMLHttpRequest to database w/ current user info.
@@ -272,61 +282,113 @@ function loadData(callback) {
     var myurl = "http://localhost:3000/submit";
     var username = prompt("Please enter your username", "");
     var email = prompt("Please enter your email",
-				"example01@tufts.edu");
+                "example01@tufts.edu");
     
     // Validate email with regex
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
     while (! re.test(email)) {
-	email = prompt("Please re-enter your email",
-				"example01@tufts.edu");
+    email = prompt("Please re-enter your email",
+                "example01@tufts.edu");
+
     }
-    
+     
     var params = "username=" + username + "&email=" + email +
-		 "&score=" + score;
-    
+        	"&score=" + score;
+     
     request.onreadystatechange = function() {
-	if (request.readyState == 4 && request.status == 200){
-	    if (typeof callback === 'function')
-		callback(request.responseText);
-	}
+        if (request.readyState == 4 && request.status == 200){
+            if (typeof callback === 'function') {
+                callback(request.responseText);
+            }
+        }
     }
-    
+     
     request.open('POST', myurl, true);
     request.setRequestHeader("Content-type",
-			     "application/x-www-form-urlencoded");
-    console.log("sending params: " + params);
+                  "application/x-www-form-urlencoded");
     request.send(params);
 }
-
+ 
 function askQ1(p, q){
-	if (q1asked == false ){
-		console.log("attempting to ask question 1");
-		q1asked = true;
-		askQuestion();
-	}
+    if (q1asked == false ){
+        q1asked = true;
+        q      = 'I have a question for you! \nIn impcore operational semantics, \nwhat kind of environment does \nphi represent?';
+        wrong1 = 'Global Variable';
+        wrong2 = 'Formal Variable';
+        right  = 'Function';
+        askQuestion(q, wrong1, wrong2, right, 'norman');
+    }
 }
 
 function askQ2(p, q){
-	if (q2asked == false) {
-		console.log("attempting to ask question 2");
-		q2asked = true;
-		askQuestion();
-	}
+    if (q2asked == false) {
+        q2asked = true;
+        q      = "Mwahaha I've got a good question!\nWhen making server-side code \nwhen is it a good idea to trust \nuser input?";
+        wrong1 = "Always! It's safe!";
+        wrong2 = "Sometimes - its not very serious";
+        right  = 'Never!';
+        askQuestion(q, wrong1, wrong2, right, 'ming');
+    }
 }
 
-function askQuestion(){
-    // Then add the menu
-             menu = game.add.sprite(w/2, h/2, 'answeroption');
-             menu.anchor.setTo(0.5, 0.5);
+function pauseGame(){
+    timer.pause();
+    game.camera.follow(null);
+}
 
-             // And a label to illustrate which menu item was chosen.
-             choiseLabel = game.add.text(w/2, h-150, 'Click outside menu to continue', { font: '30px Arial', fill: '#fff' });
-             choiseLabel.anchor.setTo(0.5, 0.5);
+function unPausegame(){
+    timer.resume();
+    game.camera.follow(player);
+}
+
+function askQuestion(quest, wr1, wr2, right, prof){
+    // stop the game timer and player movement
+    pauseGame();
+
+    var menu = game.add.sprite(game.camera.x + 3*(game.camera.width) / 4, h/2, 'answeroption');
+    menu.scale.setTo(1.71,1.71);
+
+    menu.anchor.setTo(0.5, 0.5);
+
+    var prof = game.add.sprite(game.camera.x + 70, 197, prof);
+    prof.anchor.setTo(0.5,0.5);
+
+
+
+    var introText = game.add.text(game.camera.x + 3*(game.camera.width) / 4, 130, quest, {font:'24px Arial', fill:'#000'});
+    introText.anchor.setTo(.5, .5);
+
+    var wrong1 = game.add.text(game.camera.x + 1*(game.camera.width) / 2 + 15, 240, wr1, {font:'24px Arial', fill:'#000'});
+    wrong1.inputEnabled = true;
+    wrong1.events.onInputDown.add(function(){wrongAnswer(introText)});
+
+    var wrong2 = game.add.text(game.camera.x + 1*(game.camera.width) / 2 +15, 300, wr2, {font:'24px Arial', fill:'#000'});
+    wrong2.inputEnabled = true;
+    wrong2.events.onInputDown.add(function(){wrongAnswer(introText)});
+
+    var rightAns = game.add.text(game.camera.x + 1*(game.camera.width) / 2 + 15, 360, right, {font:'24px Arial', fill:'#000'});
+    rightAns.inputEnabled = true;
+    rightAns.events.onInputDown.add(function(){
+        introText.alpha = 0;
+        menu.alpha = 0;
+        prof.alpha = 0;
+        wrong1.alpha = 0;
+        wrong2.alpha = 0;
+        rightAns.alpha = 0;
+        unPausegame();
+    });
+}
+
+function wrongAnswer(text1){
+    text1.text = "Wrong! Let's try again!";
 }
 
 function winGame(){
-	console.log("you win!!!");
+    if(finished == false){
+        finished = true;
+        endGame();
+    }
 }
 
 function update() {
